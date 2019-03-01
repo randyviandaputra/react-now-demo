1ba753b6ddcfa7c8ab0a48414efdeadc7ce44ba4
Sam Selikoff <sam.selikoff@gmail.com>
2019-03-01 12:32:41 -0500

    Adds Mirage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Server, Model, Factory, JSONAPISerializer } from '@miragejs/server';
+
+new Server({
+  models: {
+    user: Model
+  },
+  factories: {
+    user: Factory.extend({
+      name(i) {
+        return `User ${i}`;
+      }
+    })
+  },
+  serializers: {
+    application: JSONAPISerializer
+  },
+  scenarios: {
+    default(server) {
+      server.createList('user', 100);
+    }
+  },
+  baseConfig() {
+    this.namespace = 'api';
+    this.get('/users');
+    this.passthrough();
+  }
+});
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/users')
+      .then(response => response.json())
+      .then(json => this.setState({
+        users: json.data,
+        isLoading: false
+      }));
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Hello, Now + React + Mirage!</h1>
+        {this.state.isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <ul>
+            {this.state.users.map(user =>
+              <li key={user.id}>
+                {user.attributes.name}
+              </li>
+            )}
+          </ul>
+        )}
       </div>
     );
   }
